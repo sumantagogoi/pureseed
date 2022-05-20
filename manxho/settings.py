c260f1008ff83c0bfe9e48dfaf6b9758a3f0fbcb
ABDUL RASID <abdulrasid@ABDULs-MacBook-Pro.local>
2022-05-21 00:21:23 +0530

    fiexed static root path

--- manxho/settings.py
+++ manxho/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 MEDIA_ROOT = 'static/images'
 MEDIA_URL = '/images/'
