779f0b08c5170ea65de4a61a81d2005d8040ae35
ABDUL RASID <abdulrasid@ABDULs-MacBook-Pro.local>
2022-05-21 23:11:48 +0530

    added category to admin.py file

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Order, OrderItem, ShippingAddress
+from .models import Product, Order, OrderItem, ShippingAddress, Category
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
+admin.site.register(Category)
