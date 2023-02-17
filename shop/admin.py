8537187529c56345f3c286efb66830ba53a9c5ec
Sumanta Gogoi <sumanta@xynocast.com>
2023-02-18 03:35:34 +0530

    no message

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Coupons, Product, Order, OrderItem, ShippingAddress, Category, Coupons
+from .models import Coupons, Product, Order, OrderItem, ShippingAddress, Category, Coupons, \
+    PinCode
 # Register your models here.
 
 
@@ -16,6 +17,7 @@ admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
 admin.site.register(Category)
 admin.site.register(Coupons,CouponAdmin)
+admin.site.register(PinCode)
 
 
 
