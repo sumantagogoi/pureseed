7172a69a42f26b35621e5985b87b9690c00c2b89
Sumanta Gogoi <sumanta@xynocast.com>
2023-02-27 22:01:13 +0530

    no message

--- shop/models.py
+++ shop/models.py
@@ -57,7 +57,7 @@ class Product(models.Model):
 
 
     def __str__(self):
-        return self.title+' - '+self.qty+' '+self.size
+        return self.title+' - '+str(self.qty)+' '+str(self.size)
 
 
 class Order(models.Model):
