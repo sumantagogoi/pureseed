1e038147c4ffe2991a52007131344c05104c38d4
Sentinelhz <sentinelhz344@gmail.com>
2023-03-19 02:54:01 +0530

    no message

--- shop/forms.py
+++ shop/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from .models import PinCode
+from django.core.validators import RegexValidator
 
 class PinCodeForm(forms.ModelForm):
+    pincode = forms.CharField(
+        max_length=6,
+        validators=[RegexValidator(r'^\d{6}$', message='Pincode must be a 6-digit number')]
+    )
     class Meta:
         model = PinCode
         fields = '__all__'
